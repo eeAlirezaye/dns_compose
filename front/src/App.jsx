a3e6a3d146a3103901d6c80a3293876e2a65840a
eeAlirezaye <alireza.pour.ap@google.com>
2025-06-03 05:30:36 +0000

    Update the Nginx & App.jsx for proper connections

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [history, setHistory] = useState([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
-  const API_URL = import.meta.env.VITE_API_URL
+  const API_URL = '/api'
 
   const handleSubmit = async (e) => {
     e.preventDefault()
